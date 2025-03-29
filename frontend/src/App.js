0c50c34c2b99e06c481ec1d74f3da5b46e7ca2da
MikeRbl <maicoalf77@gmail.com>
2025-03-28 21:11:34 -0600

    creacion de directorio de vistas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import MapView from './components/MapView';
-import ReportForm from './components/ReportForm';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <ReportForm />
-      <MapView />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
